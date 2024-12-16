34194b15e4858d95693cd04ab3fca9b48b2155dc
Adibhange <bhangeaditya13@gmail.com>
2024-12-16 14:04:12 +0530

    Create middleware for error handling and unsupported routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware";
 
 dotenv.config();
 
@@ -17,6 +18,13 @@ main()
 async function main() {
 	await mongoose.connect(process.env.MONGO_URL);
 }
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// Middleware for handling errors
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
